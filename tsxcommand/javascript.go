1ce1e4826af890d12d5aec12c0edbd7b4639458f
Hoxca <hugh@atosc.org>
2020-11-12 03:07:51 +0100

    fix and indent javascript command

--- tsxcommand/javascript.go
+++ tsxcommand/javascript.go
@@ -1,3 +1,5 @@
+// Package tsxcommand define const and functions
+// for Software Bisque TheSkyX javascript API
 package tsxcommand
 
 const statusCommand = `
@@ -12,72 +14,98 @@ const statusCommand = `
       } else {
 			  Out = 'Mount is not parked';
 			}
-		} `
+		}`
 
-const isMountConnected = `
-/* Java Script */
-var Out;
+// IsMountConnected javascript function
+const IsMountConnected = `
+   /* Java Script */
+   var Out;
 
-if (sky6RASCOMTele.IsConnected != 0) {
-	Out = 'OK, Mount is connected';
-}`
+   if (sky6RASCOMTele.IsConnected != 0) {
+   	Out = 'OK, Mount is connected';
+   }`
 
-const isMountParked = `
-/* Java Script */
-var Out;
+// IsMountParked javascript function
+const IsMountParked = `
+   /* Java Script */
+   var Out;
 
-if (sky6RASCOMTele.IsParked()) {
-	Out = 'OK, Mount at park position';
-}`
+   if (sky6RASCOMTele.IsParked()) {
+      Out = 'OK, Mount is parked';
+   }`
 
-const isMountTracking = `
-/* Java Script */
-var Out;
+// IsMountAtPark javascript function
+const IsMountAtPark = `
+   /* Java Script */
+   var Out;
 
-if (sky6RASCOMTele.IsTracking != 0)  {
-	Out = 'OK, Mount is tracking'
-}`
+   if (sky6RASCOMTele.IsParked()) {
+      Out = 'OK, Mount is parked';
+   } else {
+      sky6RASCOMTele.GetAzAlt()
+      if ( Math.round(sky6RASCOMTele.dAlt-0.1) == 0 && Math.round(sky6RASCOMTele.dAz+0.1) == 360 && sky6RASCOMTele.IsTracking == 0 ) {
+        Out = 'OK, Mount stopped at park1 position';
+			} else if (sky6RASCOMTele.IsTracking != 0) {
+			  Out = 'Nope!, mount is tracking';
+			} else {
+        Out = 'Nope!, \nAlt: '+Math.round(sky6RASCOMTele.dAlt-0.1)+'\nAz : '+Math.round(sky6RASCOMTele.dAz+0.1);
+			}
+	 }`
 
-const isMountStopped = `
-/* Java Script */
-var Out;
+// IsMountTracking javascript function
+const IsMountTracking = `
+   /* Java Script */
+   var Out;
 
-if (sky6RASCOMTele.IsTracking == 0 || sky6RASCOMTele.IsParked())  {
-	Out = 'OK, Mount is stopped'
-}`
+   if (sky6RASCOMTele.IsTracking != 0)  {
+   	Out = 'OK, Mount is tracking'
+   }`
 
-const getMountPosition = `
-/* Java Script */
-var Out;
+// IsMountStopped javascript function
+const IsMountStopped = `
+   /* Java Script */
+   var Out;
 
-sky6RASCOMTele.GetAzAlt()
- Out = 'Alt: '+sky6RASCOMTele.dAlt+'\nAz : '+sky6RASCOMTele.dAz
-`
+   if (sky6RASCOMTele.IsTracking == 0 || sky6RASCOMTele.IsParked())  {
+   	Out = 'OK, Mount is stopped'
+   }`
 
-const getCameraCoolerStatus = `
-var Out, Reg;
-Reg = (ccdsoftCamera.RegulateTemperature == 1) ? "On":"Off"
+// GetMountPosition javascript function
+const GetMountPosition = `
+   /* Java Script */
+   var Out;
 
-Out = 'Temp : '+ccdsoftCamera.Temperature+'\nPower: '+ccdsoftCamera.ThermalElectricCoolerPower+'\nRegulation: '+Reg
-`
+   sky6RASCOMTele.GetAzAlt()
+   Out = 'Alt: '+Math.round(sky6RASCOMTele.dAlt-0.1)+'\nAz : '+Math.round(sky6RASCOMTele.dAz+0.1)`
 
-const isCoolerOff = `
-var Out, Reg;
+// GetCameraCoolerStatus javascript function
+const GetCameraCoolerStatus = `
+   /* Java Script */
+   var Out, Reg;
+   Reg = (ccdsoftCamera.RegulateTemperature == 1) ? "On":"Off"
 
-if (ccdsoftCamera.RegulateTemperature == 0) {
-	Out = "Ok, Cooler power: "+ccdsoftCamera.ThermalElectricCoolerPower+"% Regulation: off"
-} else {
-  Reg = (ccdsoftCamera.RegulateTemperature == 1) ? "On":"Off"
-	Out = "Nope!, Cooler power: "+ccdsoftCamera.ThermalElectricCoolerPower+"% Regulation: "+Reg
-}
-`
-const parkAndDisconnectCommand = `
-	  /* Java Script */
-	  var Out;
-	  if (sky6RASCOMTele.IsParked()) {
-      sky6RASCOMTele.Unpark();
-      sky6RASCOMTele.Park();
-    } else {
-      sky6RASCOMTele.Park();
-		}
-	  Out = sky6RASCOMTele.IsConnected; `
+   Out = 'Temp : '+ccdsoftCamera.Temperature+'\nPower: '+ccdsoftCamera.ThermalElectricCoolerPower+'%\nRegulation: '+Reg`
+
+// IsCoolerOff javascript function
+const IsCoolerOff = `
+   /* Java Script */
+   var Out, Reg;
+
+   if (ccdsoftCamera.RegulateTemperature == 0) {
+   	Out = "Ok, Cooler power: "+ccdsoftCamera.ThermalElectricCoolerPower+"% Regulation: off"
+   } else {
+     Reg = (ccdsoftCamera.RegulateTemperature == 1) ? "On":"Off"
+   	Out = "Nope!, Cooler power: "+ccdsoftCamera.ThermalElectricCoolerPower+"% Regulation: "+Reg
+   }`
+
+// ParkAndDisconnectCommand javascript function
+const ParkAndDisconnectCommand = `
+   /* Java Script */
+   var Out;
+   if (sky6RASCOMTele.IsParked()) {
+     sky6RASCOMTele.Unpark();
+     sky6RASCOMTele.Park();
+   } else {
+     sky6RASCOMTele.Park();
+   }
+   Out = sky6RASCOMTele.IsConnected;`
